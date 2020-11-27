97eab36237d369066f81c4d924171707674c82c5
joenjihwan <s20062@gsm.hs.kr>
2020-11-27 20:14:47 +0900

    [UPDATE] header design.

--- reservation-room/src/components/MainPage/MainPage.js
+++ reservation-room/src/components/MainPage/MainPage.js
@@ -8,6 +8,11 @@ class MainPage extends Component {
         <div class="logo_wrap">
           <img src={ Logo } alt="로고"></img>
         </div>
+        <div class="btn_wrap">
+          <button>조회하기</button>
+          <button>신청하기</button>
+          <button>로그인</button>
+        </div>
       </Main.Header>
     );
   }
--- reservation-room/src/components/MainPage/Style.js
+++ reservation-room/src/components/MainPage/Style.js
@@ -5,7 +5,25 @@ export const Header = styled.div `
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+    }  
+    width: 100%;
+    height: 97px;
+    background-color: #8CAE7E;
+    display: flex;
+    justify-content: space-between;
+    .btn_wrap{
+        width: 800px;
+        margin-top: 16px;
     }
-    background-color: #999999;
+    .btn_wrap > button{
+        width: 184px;
+        height: 65px;
+        margin-left: 65px;
+        font-size: 25px;
+        border: 3px solid white;
+        background: none;
+        color: #ffff;
+        border-radius: 10px;
+    }
 `;
 
