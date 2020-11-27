b03f895866f097cbeb4555761962666080137b89
joenjihwan <s20062@gsm.hs.kr>
2020-11-27 22:48:40 +0900

    오늘은 여기까지.

--- reservation-room/src/components/Templates/JoinLoginTemp.js
+++ reservation-room/src/components/Templates/JoinLoginTemp.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import * as Template from "./Style";
+import GreenLogo from "../images/GreenLogo.png";
+import Read from "../images/read.png";
 
 class JoinLoginTemp extends Component {
     render() {
         return(
             <Template.Page>
-                <div></div>
+                <div class="white_box">
+                    <div class="content_wrap">
+                        <img src={GreenLogo} alt="로고"></img>
+                        <img src={Read} alt="읽는중"></img>
+                    </div>
+                </div>
             </Template.Page>
         );
     }
--- reservation-room/src/components/Templates/Style.js
+++ reservation-room/src/components/Templates/Style.js
@@ -5,8 +5,20 @@ export const Page = styled.div`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        width: 100%;
-        height: 100%;
     }
-    background-color: #DAF2D1;
+    width: 100%;
+    height: 1080px;
+    background: #DAF2D1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .white_box{
+        width: 1419px;
+        height: 840px;
+        background: #ffff;
+    }
+    .content_wrap{
+        flex-direction: column;
+    }
 `;
