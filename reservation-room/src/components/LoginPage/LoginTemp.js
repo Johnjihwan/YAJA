7991b21c13d76929a9d17d965d147fbd69ba83a0
joenjihwan <s20062@gsm.hs.kr>
2020-11-30 20:14:41 +0900

    [UPDATE] login function complete.

--- reservation-room/src/components/LoginPage/LoginTemp.js
+++ reservation-room/src/components/LoginPage/LoginTemp.js
@@ -3,32 +3,51 @@ import * as Login from "./Style";
 
 export default class LoginTemp extends Component {
   state = {
-    name: '',
-    phone: ''
-  }
+    email: "",
+    password: "",
+  };
   handleChange = (e) => {
+    // 이 타겟의 name : 이 타겟의 값.
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
-  }
+    // 로그인에 관한 데이터는 this.state 를 쓰시면 됩니다.
+    console.log(this.state);
+  };
   render() {
     return (
-        <Login.Section>
+      <Login.Section>
+        <form onSubmit={this.handleSubmit}>
           <div class="Login_wrap">
             <div class="Login_container">
-                <a href="null">로그인</a>
+              <a href="null">로그인</a>
             </div>
             <div class="login_meterial">
-              <input type="email" placeholder="이메일" value={this.state.name} onChange={this.handleChange} name="email"></input> <br/>
-              <input type="password" placeholder="비밀번호" value={this.state.phone} onChange={this.handleChange} name="password"></input>
+              <input
+                type="email"
+                placeholder="이메일"
+                value={this.state.name}
+                onChange={this.handleChange}
+                name="email"
+              ></input>{" "}
+              <br />
+              <input
+                type="password"
+                placeholder="비밀번호"
+                value={this.state.phone}
+                onChange={this.handleChange}
+                name="password"
+              ></input>
             </div>
-            <button class="btn">로그인</button>
+            <button type="submit" class="btn">
+              로그인
+            </button>
             <div class="more">
               <a href="null">아직 계정이 없으신가요?</a>
             </div>
           </div>
-          <div>{this.state.name} {this.state.phone}</div>
-        </Login.Section>
+        </form>
+      </Login.Section>
     );
   }
 }
